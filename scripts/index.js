5a38c1482c8d2ae70cdd9f6138e658ea6b68a28a
largentba21 <largentba21@gmail.com>
2021-09-06 17:14:52 -0500

    Getting the collapsible sections workign

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,87 @@
-Hooks.on("renderJournalSheet", (app, html, data) => {
-  //   console.log(app);
-  //   console.log(html);
-  //   console.log(data);
+let headerIdMap = {};
 
-  const buildNextUntilSelector = (tag) => {
-    let selector = "";
-    let level = Number.parseInt(tag.slice(1), 10);
-    for (; level > 0; level--) {
-      selector += `h${level}`;
-      if (level > 1) {
-        selector += ",";
-      }
+const buildNextUntilSelector = (tag) => {
+  let selector = "";
+  let level = Number.parseInt(tag.slice(1), 10);
+  for (; level > 0; level--) {
+    selector += `h${level}`;
+    if (level > 1) {
+      selector += ",";
     }
-    return selector;
-  };
+  }
+  return selector;
+};
 
+const childElRecursion = ({ childEls, isOpen }, topIsOpen) => {
+  childEls.forEach((childHtml) => {
+    childHtml.classList[topIsOpen ? (isOpen ? "remove" : "add") : "add"](
+      "cje-d-none"
+    );
+    if (childHtml.dataset.cjeId) {
+      childElRecursion(headerIdMap[childHtml.dataset.cjeId], topIsOpen);
+    }
+  });
+};
+
+const collectChildElements = (el, selector) => {
+  const headerTags = { H1: 1, H2: 1, H3: 1, H4: 1, H5: 1, H6: 1 };
+  const siblings = [];
+  let foundSubHeader = "";
+  el = el.nextElementSibling;
+
+  while (el) {
+    if (el.matches(selector)) {
+      break;
+    }
+
+    if (!foundSubHeader) {
+      if (headerTags[el.tagName]) {
+        foundSubHeader = el.tagName;
+      }
+      siblings.push(el);
+    } else if (el.tagName === foundSubHeader) {
+      siblings.push(el);
+    }
+
+    el = el.nextElementSibling;
+  }
+
+  return siblings;
+};
+
+Hooks.on("closeJournalSheet", () => {
+  headerIdMap = {};
+});
+
+Hooks.on("renderJournalSheet", (app, html, data) => {
   const headers = html.find(".editor").find("h1,h2,h3,h4,h5,h6");
-  headers.each(function (_, html) {
-    html.innerText = `!!! ${html.innerText}`;
+  headers.each(function (idx, html) {
+    const id = `cje-${idx}`;
+    html.dataset.cjeId = id;
     html.classList.add("cje-collapsible");
     const el = $(this);
+    console.log(el.html());
+    el.html(
+      `<i data-feather='plus-circle'></i><i data-feather='minus-circle'></i>${el.html()}`
+    );
+    const childEls = collectChildElements(
+      html,
+      buildNextUntilSelector(html.tagName)
+    );
     const _handleClick = () => {
-      const childEls = el.nextUntil(buildNextUntilSelector(html.tagName));
-      return () => {
-        console.log("clicked!");
-        console.log(el);
-        console.log(childEls);
-      };
+      headerIdMap[id].isOpen = !headerIdMap[id].isOpen;
+      headerIdMap[id].el[0].classList[
+        headerIdMap[id].isOpen ? "remove" : "add"
+      ]("cje-collapsed");
+      childElRecursion(headerIdMap[id], headerIdMap[id].isOpen);
     };
-    el.on("click", _handleClick());
+    headerIdMap[id] = {
+      el,
+      childEls,
+      _handleClick,
+      isOpen: true, // TODO
+    };
+    el.on("click", _handleClick);
   });
+  feather.replace();
 });
